bdddd352198c89f344eba6703612558cb444c7a6
shyamraja <t7bhsh00@students.oamk.fi>
2019-03-31 16:48:16 +0300

    loginform added

--- src/components/landing/UploadDocumentForm.js
+++ src/components/landing/UploadDocumentForm.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React,{ Component } from "react";
+import InputFormField from "../../common/InputFormField";
 //import PropTypes from "prop-types";
+import PrimaryButton from "../../common/PrimaryButton";
 
 const UploadDocumentForm = props => {
+  
   return (
-    <div>
-      <p>Upload Document Form Here</p>
-    </div>
+    <div className="upload-document-form">
+      <p>YOU MUST LOGIN TO UPLOAD YOUR THESIS</p>
+      <form>
+        <div className="form-actions side-by-side">
+          <InputFormField label="USERNAME/EXAM REGISTRATION NUMBER" name="USERNAME/EXAM REGISTRATION NUMBER" /> 
+          <InputFormField label="PASSWORD" name="PASSWORD"/>
+         </div>
+      
+          
+         <PrimaryButton type="submit">LOGIN</PrimaryButton>
+             
+    </form>
+    </div>  
+    
   );
 };
+  UploadDocumentForm.propTypes = {};
+  
+  export default UploadDocumentForm;
+  
 
-UploadDocumentForm.propTypes = {};
 
-export default UploadDocumentForm;
+
+
+
